e8bb4e8b94a349477fb8cff6d9e0aee1d372bfb0
Mike-415 <mgonz160@mail.ccsf.edu>
2023-03-04 15:49:19 -0800

    create the list item card

--- app/src/main/java/com/example/affirmationapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationapp/MainActivity.kt
@@ -3,20 +3,29 @@ package com.example.affirmationapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationapp.model.Affirmation
 import com.example.affirmationapp.ui.theme.AffirmationAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AffirmationApp()
+            AffirmationAppTheme() {
+                AffirmationApp()
+            }
         }
     }
 }
@@ -24,8 +33,27 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun AffirmationApp() {
-    AffirmationAppTheme {
+    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
+}
 
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = Modifier.padding(8.dp), elevation = 4.dp) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.h6
+            )
+        }
     }
 }
 
