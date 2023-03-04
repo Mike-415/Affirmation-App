a601b0222cc144d467dd54958a7566096e26c03c
Mike-415 <mgonz160@mail.ccsf.edu>
2023-03-04 15:32:36 -0800

    added data and model

--- app/src/main/java/com/example/affirmationapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationapp/MainActivity.kt
@@ -16,28 +16,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AffirmationAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            AffirmationApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun AffirmationApp() {
     AffirmationAppTheme {
-        Greeting("Android")
+
     }
 }
+
